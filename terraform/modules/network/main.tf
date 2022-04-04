948ee5aaa5f60af85a5da764cd6316e675a34509
mansimran <mansimran@kaur>
2022-04-04 02:37:16 +0000

    testing

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  version = "~>3.0"
+  #version = "~>3.0"
   region  = "us-east-1"
+  access_key = "ASIAWN5MDKSJZEIFUUGA"
+  secret_key = "Y5KOIhC2aEU29j0wPaHvsUqadBnLpQnFJh12ia0n"
 }
 
 data "aws_availability_zones" "available" {
