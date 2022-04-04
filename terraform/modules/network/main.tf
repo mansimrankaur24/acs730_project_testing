fa47f83c332621ab6d143843fa0dd8408a7e81f6
mansimran <mansimran@kaur>
2022-04-04 02:22:27 +0000

    updating terrform and aws versions

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+}
 provider "aws" {
-  region = "us-east-1"
+  version = "~>3.0"
+  region  = "east-us-1"
 }
 
 data "aws_availability_zones" "available" {
