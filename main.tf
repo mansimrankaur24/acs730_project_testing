78a6600721e9eef3db2df98e43d7155e217204ed
mansimrankaur24 <99369662+mansimrankaur24@users.noreply.github.com>
2022-04-06 00:18:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
