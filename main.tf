f925cbd3e782ab3358ff15b7b1774285df4975ce
mansimrankaur24 <99369662+mansimrankaur24@users.noreply.github.com>
2022-04-06 00:17:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "3.26.0"
     }
   }
-
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
